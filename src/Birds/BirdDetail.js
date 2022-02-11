ec5899bcb94187275befd999ae35c6a35c57cf2b
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-11 12:09:44 -0800

    detail page working

--- src/Birds/BirdDetail.js
+++ src/Birds/BirdDetail.js
@@ -1,7 +1,28 @@
-
+import { useEffect, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import { getBird } from '../services/fetch-utils';
+import './Birds.css';
 
 export default function DetailPage() {
+  const [bird, setBird] = useState({});
+  let match = useRouteMatch('/birds/:id');
+
+  useEffect(() => {
+    async function fetch() {
+      const thisBird = await getBird(match.params.id);
+
+      setBird(thisBird);
+    }
+    fetch();
+  }, [match.params.id]);
+
   return (
-    <div>DetailPage</div>
+    <div className='bird-detail'>
+      <p>{bird.name}</p>
+      <p>{bird.species}</p>
+      <p>seen: {bird.date}</p>
+      <p>location: {bird.location}</p>
+      <p>notes: {bird.notes}</p>
+    </div>
   );
 }
