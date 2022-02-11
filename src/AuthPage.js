1eb1c00a430de3d6c6c10d36d92cc73e1f2ee721
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-11 10:31:59 -0800

    auth form rendering

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+import { signIn, signUp } from './services/fetch-utils';
+
+export default function AuthPage({ setUser }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
 
-export default function AuthPage() {
+  async function handleSignIn(e) {
+    e.preventDefault();
+
+    const returningUser = await signIn(email, password);
+
+    setUser(returningUser);
+    setEmail('');
+    setPassword('');
+  }
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+
+    const newUser = await signUp(email, password);
+
+    setUser(newUser);
+    setEmail('');
+    setPassword('');
+  }
+
   return (
-    <div>AuthPage</div>
+    <div className='auth'>
+      <form>
+        <input required type='email' value={email}
+          placeholder='email'
+          onChange={(e) => setEmail(e.target.value)} />
+        <input required type='password' value={password}
+          placeholder='password'
+          onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={handleSignIn}>sign in</button>
+        <button onClick={handleSignUp}>sign up</button>
+      </form>
+    </div>
   );
 }
