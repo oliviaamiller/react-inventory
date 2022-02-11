4f3052da66676f6b99f940cce4d678dfd839b25b
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-11 11:15:38 -0800

    passed props to bird form

--- src/Birds/CreateBird.js
+++ src/Birds/CreateBird.js
@@ -1,6 +1,46 @@
+import { useState } from 'react';
+import { createBird } from '../services/fetch-utils';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import BirdForm from './BirdForm';
+import './Birds.css';
+
 
 export default function CreatePage() {
+  const [name, setName] = useState('');
+  const [species, setSpecies] = useState('');
+  const [date, setDate] = useState('');
+  const [location, setLocation] = useState('');
+  const [notes, setNotes] = useState('');
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    await createBird({
+      name, 
+      species,
+      date,
+      location,
+      notes
+    });
+
+    history.push('./birds');
+  }
+
   return (
-    <div>CreatePage</div>
+    <div className='create-bird'>
+      <BirdForm
+        submitBird={handleSubmit}
+        name={name}
+        setName={setName}
+        species={species}
+        setSpecies={setSpecies}
+        date={date}
+        setDate={setDate}
+        location={location}
+        setLocation={setLocation}
+        notes={notes}
+        setNotes={setNotes} />
+    </div>
   );
 }
